942076862781a04ae74fb2a6adf8344773f79b4e
AlaYousef <>
2023-07-14 09:57:36 +0000

    add a new host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = 'django-insecure-=-wn*x27_vn$p15il!o3qt(nq4*e0w&n7bm73lw-5ha_i9@c0+
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alayousef-full-stack-fra-s85jo8xb95.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['alaa-django-todo-app-961d9b8f3769.herokuapp.com']
 
 
 # Application definition
